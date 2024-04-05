08052cfb2861203b4846657eceedeff0046de1db
VitroidFPV <vitroid05@gmail.com>
2024-04-05 09:32:38 +0200

    Content: Why

---
+++ src/routes/why/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+
+<div class="container h-full mx-auto flex flex-col justify-center items-center">
+	<div class="w-full p-16 text-xl">
+		<h1 class="text-6xl text-secondary-400 mb-4 font-bold">What is This?</h1>
+		<p>
+			ELRS is an amazing project. Its high performance and low cost make it a great choice for many applications.
+			<br><br>
+			That has however made it attractive for many kinds of misuse, be it for illegal, unsafe or unethical 
+			purposes. Users and even manufacturers seek to gain an advantage or even profit by using ELRS in ways 
+			that are not condoned, supported, or aided by the community and the development team.
+		</p>
+		<br><br>
+		<p>
+			This is a very real issue that I wanted to shine the light to... in the silliest way that I could come 
+			up with.
+			<br><br>
+			Every time a question related to "incorrect" use of ELRS in the ELRS Discord server comes up and 
+			gets reported, the timer resets. Job safety accident prevention style.
+			<br><br>
+			Please don't take this seriously.
+		</p>
+		<div class="flex w-full justify-center">
+			<img
+				src="https://c.tenor.com/eZNP54sNYWMAAAAC/tenor.gif"
+				alt="0 Days Since Last Incident GIF"
+				class="rounded-lg w-1/3 mt-4"
+			>
+		</div>
+	</div>
+</div>
